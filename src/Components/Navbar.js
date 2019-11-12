61ea15fbf2b3aebe7b6dc8ed720f717b57d54bdd
Shivaraj Bakale <shivarajapple@Shivarajs-MacBook-Pro.local>
2019-11-12 19:35:58 +0530

    Gradients added

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,8 +3,15 @@ import './style.css';
 
 const Navbar = () => {
     return (
-        <nav class="navbar navbar-light bgColor">
-            <p>TodoApplication</p>
+        <nav class="navbar navbar-light bgColor spaceBetweenCss">
+            <div>
+            <h3 style={{ color: 'white' }}>TodoApplication</h3>
+            </div>
+            <div className="row">
+                <p>About</p>
+                <p>Services</p>
+                <p>Project</p>
+            </div>
         </nav>
     );
 }
